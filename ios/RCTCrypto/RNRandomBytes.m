3e2b8c9afc6942b44cfa1ed449ecfd21af3ddd34
mbidefi <info@mbidefi.io>
2021-07-05 01:43:08 +0800

    start

--- ios/RCTCrypto/RNRandomBytes.m
+++ ios/RCTCrypto/RNRandomBytes.m
@@ -7,49 +7,9 @@
 //
 
 #import "RNRandomBytes.h"
-#if __has_include(<React/RCTBridgeModule.h>)
-#import <React/RCTBridgeModule.h>
-#elif __has_include("RCTBridgeModule.h")
-#import "RCTBridgeModule.h"
-#else
-#import "React/RCTBridgeModule.h" // Required when used as a Pod in a Swift project
-#endif
-
 @implementation RNRandomdBytes
 
 RCT_EXPORT_MODULE()
 
-@synthesize bridge = _bridge;
-
-RCT_EXPORT_METHOD(randomBytes:(NSUInteger)length
-                  resolver:(RCTPromiseResolveBlock)resolve
-                  rejecter:(RCTPromiseRejectBlock)reject)
-{
-    NSError *error = nil;
-    NSString *base64 = [RNRandomBytes randomBytes:length];
-    if (base64 == nil) {
-        reject(@"random_bytes failed", @"Random bytes error", error);
-    } else {
-        resolve(base64);
-    }
-}
-
-+ (NSString *) randomBytes:(NSUInteger)length
-{
-    NSMutableData* bytes = [NSMutableData dataWithLength:length];
-    SecRandomCopyBytes(kSecRandomDefault, length, [bytes mutableBytes]);
-    return [bytes base64EncodedStringWithOptions:0];
-}
-
-+ (NSDictionary *)constantsToExport
-{
-    return @{
-        @"seed": [RNRandomBytes randomBytes:4096]
-    };
-};
-
-+ (BOOL)requiresMainQueueSetup {
-    return YES;
-}
 
 @end
